fee99cd9af550726ffc41f7fc150baf3b68558cd
DoroteyaGalbacheva <dgalbacheva@gmail.com>
2023-01-24 09:07:54 +0300

    user.py

---
+++ user.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+city = input("Where do you live? ")
+print ("\n", "This is ", name, "\n", "She/He is ", age, "years old", "\n", "She/He lives in ", city)
+
+
