f75379ba1d7c24c4a980e3553dd2eb992c248083
DoroteyaGalbacheva <dgalbacheva@gmail.com>
2023-01-24 09:09:39 +0300

    numbers.py

---
+++ numbers.py
@@ -0,0 +1,9 @@
+print("Введите четири числа:  ")
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+sum1 = a + b
+sum2 = c + d
+division = sum1 / sum2
+print("Результатом деления из суммы первых двух чисел и суммы последующих двух = %.2f" %division)
