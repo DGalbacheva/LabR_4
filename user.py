c3cd2af8a52229a29b481800e7f5069bdc81ab82
DGalbacheva <113231573+DGalbacheva@users.noreply.github.com>
2023-01-25 14:44:37 +0300

    Delete user.py

--- user.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name? ")
-age = input("How old are you? ")
-city = input("Where do you live? ")
-print ("\n", "This is ", name, "\n", "She/He is ", age, "years old", "\n", "She/He lives in ", city)
-
-
